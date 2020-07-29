d60ac34488381720f37e62082140a9dd89036a98
Sergey Chausov <chouzoff@gmail.com>
2020-07-29 17:10:39 +0300

    macOS target added to Example

---
+++ Example/KalturaClient_Example_macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  KalturaClient_Example_macOS
+//
+//  Created by Sergii Chausov on 29.07.2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Example/KalturaClient_Example_macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KalturaClient_Example_macOS
+//
+//  Created by Sergii Chausov on 29.07.2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
